80f48484e77ca242fe9c81e922831225f3988e71
Mehti Musayev <musayev@selectel.ru>
2016-11-29 11:59:39 +0300

    Initial commit

---
+++ intro/first_example_slide_4.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+
+import ast
+
+from redbaron import RedBaron
+
+code = """
+class foo:
+    print 'Do you like Vodka?'
+"""
+
+red = RedBaron(code)
+
+print("This is RedBaron tree:")
+print(red.help(with_formatting=True))
+
+
+tree = ast.parse(code)
+
+print("\nThis is Python AST:\n")
+
+
+print(ast.dump(tree))
